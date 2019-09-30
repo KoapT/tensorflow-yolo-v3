722ba352ebd4757009dbcfd7a088f2386e65d609
koap <37614244+KoapT@users.noreply.github.com>
2019-09-30 16:47:40 +0800

    add the densenet yolov3

--- convert_weights_pb.py
+++ convert_weights_pb.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import tensorflow as tf
-import yolo_v3
+import yolo_v3, dense_yolov3_v1
 import yolo_v3_tiny
 import yolov3_tiny_3l
 from PIL import Image, ImageDraw
@@ -14,22 +14,25 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string(
     'class_names', 'bird.names', 'File with class names')
 tf.app.flags.DEFINE_string(
-    'weights_file', 'darknet_weights/yolov3-bird_final.weights', 'Binary file with detector weights')
+    'weights_file', '/home/tk/Share/darknet/weights/yolov3-dense_final.weights', 'Binary file with detector weights')
 tf.app.flags.DEFINE_string(
     'data_format', 'NHWC', 'Data format: NCHW (gpu only) / NHWC')
 tf.app.flags.DEFINE_string(
-    'output_graph', 'saved_model_pb/test.pb', 'Frozen tensorflow protobuf model output path')
+    'output_graph', 'saved_model_pb/dense_yolo.pb', 'Frozen tensorflow protobuf model output path')
 
 tf.app.flags.DEFINE_bool(
     'tiny', False, 'Use tiny version of YOLOv3')
+tf.app.flags.DEFINE_bool(
+    'dense', True, 'Use dense version of YOLOv3')
 tf.app.flags.DEFINE_integer(
     'size', 608, 'Image size')
-
 
 
 def main(argv=None):
     if FLAGS.tiny:
         model = yolov3_tiny_3l.yolo_v3_tiny
+    elif FLAGS.dense:
+        model = dense_yolov3_v1.dense_yolo_v3
     else:
         model = yolo_v3.yolo_v3
 
@@ -48,6 +51,8 @@ def main(argv=None):
     with tf.Session() as sess:
         sess.run(load_ops)
         freeze_graph(sess, FLAGS.output_graph)
+        writer = tf.summary.FileWriter("logs/", sess.graph)
+
 
 if __name__ == '__main__':
     tf.app.run()
